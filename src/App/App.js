93e55779aca9b83ac336198783393175d09bf277
Crystal Broach <crysland@gmail.com>
2019-12-12 22:38:34 -0600

    updated app file

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+
+import studentsData from '../helpers/data/studentsData';
+
 import './App.scss';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    students: [],
+  }
+
+  componentDidMount() {
+    const students = studentsData.getStudents();
+    this.setState({ students });
+  }
+
+  render() {
+    return (
     <div className="App">
+      <h1>Shark Attack</h1>
       <button className='btn btn-danger'>HELP ME</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
